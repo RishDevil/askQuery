24758f990ce6b77141fab3f335009b25fc940a79
RishDevil <49071827+RishDevil@users.noreply.github.com>
2021-03-11 23:41:03 +0530

    Update dev.js

--- utilty/dev.js
+++ utilty/dev.js
@@ -1,9 +1,9 @@
 module.exports = {
   MONGOURI:
-    "mongodb+srv://rish:CxiW1CsPeAp46oPU@cluster0.kjvxf.mongodb.net/query",
-  cloud_name: "duvsvsxhk",
-  api_key: "654997219996181",
-  api_secret: "JBSXu1uEL5oLUu0bD_lOYJ-RZxI",
-  user_EMAIL: "tempidforcheck@gmail.com",
-  pass_PASS: "rishabhcool13",
+    "uri",
+  cloud_name: "name",
+  api_key: "key1",
+  api_secret: "secret",
+  user_EMAIL: "email",
+  pass_PASS: "password",
 };
